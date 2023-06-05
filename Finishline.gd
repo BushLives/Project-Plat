7ff1dbf49f06b922ee6b29eb4aeae2262d712d1d
BushLives <Kevinl18@learn.cssd.ab.ca>
2023-06-05 11:14:16 -0600

    Finish line

--- Finishline.gd
+++ Finishline.gd
@@ -1,4 +1,12 @@
 extends Area2D
 
-func _ready():
-	connect()
+func _ready() -> void:
+	connect("body_entered", self, "_on_body_entered")
+	
+func _on_body_entered(body):
+	set_process(false)
+	if body.is_in_group("Player1"):
+		body.set_physics_process(false)
+
+func finish_game(body):
+	body.GUI.Visible = false
--- Game_over.gd
+++ Game_over.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_Play_Again_pressed():
-	get_tree().change_scene("res://World10.tscn")
+	get_tree().change_scene("res://test.tscn")
 
 
 func _on_MainMenu_pressed():
