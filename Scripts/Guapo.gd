3f1aafabad2b2591016dd33fcf1148c972e46c46
BushLives <BurningLight672@gmail.com>
2023-06-15 15:55:20 -0600

    e

---
+++ Scripts/Guapo.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+onready var timer = $Timer
+onready var flashbang = $White
+onready var Insta = $Area2D
+onready var sound = $AudioStreamPlayer
+
+var counter = 0
+var entered = 0
+
+func _on_Cheese_body_entered(body):
+	if body.is_in_group("Player1"):
+		print("Has fallen into a trap!")
+		timer.wait_time = 1.25
+		timer.start()
+		entered += 1
+		if body.is_in_group("Player1") and entered >= 1 and counter >= 1:
+			body.attack_detech(99999)
+			
+				
+
+func _on_Timer_timeout():
+	if entered == 1:
+		
+		flashbang.visible = true
+		Insta.monitoring = true
+		if counter == 0 :
+			counter += 1
+			sound.play()
